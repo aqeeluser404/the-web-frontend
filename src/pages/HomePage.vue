a0054ccf4c540953d4478a4ca91904c42f4a262f
Aqeel <aqeelhanslo@gmail.com>
2025-02-04 16:58:42 +0200

    updated homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -54,7 +54,6 @@
           <CustomButton v-else color="brown" customStyle="width: 100%" text-color="white" label="Make an Enquiry" @click="scrollToSection('contact-section')" />
         </q-card-section>
       </q-card>
-
       <div class="col-md-5 col-12">
         <q-img src="~src/assets/resources/home/amenities/About.jpeg" alt="hero image" class="side-image"/>
       </div>
@@ -62,18 +61,17 @@
 
     <!-- -----------------------------------------------------------------------------------------------------------------------------------------------  -->
     <!-- images -->
-    <div id="images-section" style="height: 100%; background-color: white;">
+    <div id="images-section" style="height: 100%; background-color: #333;">
       <q-list class="row justify-around">
-        <q-card class="q-pa-sm bg-transparent col-md-4 col-12" flat v-for="card in cards" :key="card._id">
+        <div class="bg-transparent col-md-4 col-12" flat v-for="card in cards" :key="card._id">
           <q-img :src="card.imageUrl" :alt="card.title" />
-        </q-card>
+        </div>
       </q-list>
     </div>
 
     <!-- -----------------------------------------------------------------------------------------------------------------------------------------------  -->
     <!-- academics -->
     <div id="academics-section" style="height: 100%;" class="row justify-between">
-
       <div class="col-md-5 col-12 row justify-center">
         <q-img src="~src/assets/resources/home/academics/Students.png" alt="hero image" class="side-image"/>
       </div>
@@ -107,26 +105,26 @@
     </div>
 
     <!-- academics - section 2 -->
-    <div style="height: 100%; background-color: #333;" class="q-pa-xl">
-      <q-card flat class="bg-transparent text-white text-body1 text-center constrain">
-        <q-card-section class="col-md-4 col-12">
+    <div style="height: 100%; background-color: #333;">
+      <q-card flat class="bg-transparent text-white text-body1 text-left q-pa-lg">
+
+        <q-card-section class="row">
           <div class="text-h3 q-mb-xl">Enhancing a Quaint Suburb of Stellenbosch</div>
           <div>
             Paramount to The Web's vision, is to ultimately enhance this quaint suburb by making it more pedestrian-friendly and in doing so, create a café culture atmosphere along its beautiful tree-lined streets.
             <br>In a Stellenbosch first, over R35 million has been raised and earmarked by a group of developers in the area to build towards this vision and amplify the suburb, as well as improve its accessibility to campus. The funds will be allocated to 11 projects that range from major security enhancements, to local area infrastructure upgrades for all who reside in the area.
             <br><br>This desirable location, well on its way becoming the next Stellies student hub, has likeminded developers already taking advantage of The Web's entrance into the area by purchasing existing houses in the precinct for further student development.
           </div>
-          <br><br>
-          <div class="">
-            <q-img src="~src/assets/resources/home/academics/Home.jpeg" class="smaller-image"/>
-          </div>
+        </q-card-section>
+
+        <q-card-section class="row">
+          <q-img src="~src/assets/resources/home/academics/Home.jpeg" />
         </q-card-section>
       </q-card>
     </div>
 
     <!-- academics - section 3 -->
     <div class="row justify-between" style="height: 100%;">
-
       <q-card flat class="
         bg-transparent
         text-body1
@@ -148,7 +146,6 @@
           <CustomButton v-else color="brown" text-color="white" customStyle="width: 100%" label="Secure your spot" @click="openUnitDash" />
         </q-card-section>
       </q-card>
-
       <div class="col-md-5 col-12 row justify-center">
         <q-img src="~src/assets/resources/home/academics/Happy-Students.png" class="side-image"/>
       </div>
@@ -156,11 +153,9 @@
 
     <!-- academics - section 4 -->
     <div class="row justify-between" style="height: 100%; background-color: #333;">
-
       <div class="col-md-5 col-12">
         <q-img src="~src/assets/resources/home/academics/Residential.png" class="side-image"/>
       </div>
-
       <q-card flat style="" class="
         bg-transparent
         text-white
@@ -181,11 +176,9 @@
     <!-- academics - section 5 -->
     <div class="q-pa-xl" style="height: 100%; background-color: #222;">
       <q-card flat class="bg-transparent text-white text-body1 text-center constrain">
-
         <q-card-section>
           <div class="text-h4">The Web's Amenities Foster an Environment Ideal for Academic Success</div>
         </q-card-section>
-
         <q-card-section class="">
           <div class="col-md-6">
             The academic advisor of The Web is a former senior director of the Division for Student Affairs of Stellenbosch University. Residents of The Web will be encouraged to participate in organised student programmes offered by The Web and the various tertiary institutions they attend, such as well-being programmes, leadership training, mentoring programmes, and social events.
@@ -193,7 +186,6 @@
           <div class="col-md-6">
             The Web is designed in collaboration with top academic experts to give your child the highest possibility of academic success. Cooperation between the tertiary institutions in Stellenbosch and The Web to the benefit of the students who will take up residence at The Web is possible.
           </div>
-
         </q-card-section>
       </q-card>
     </div>
@@ -218,30 +210,22 @@
     <!-- -----------------------------------------------------------------------------------------------------------------------------------------------  -->
     <!-- units card -->
     <div id="units-section" class="q-py-xl" style="height: 100%; background-color: #222;">
-
       <q-card-section>
         <div class="text-h4 text-center text-white">Unit Specifications</div>
       </q-card-section>
-
       <q-list class="row justify-center q-pa-sm">
         <q-card class="q-pa-md bg-transparent col-md-3 col-12" flat v-for="unitType in unitTypes" :key="unitType._id">
-
           <q-img class="" :src="unitType.imageUrl" />
           <div class="text-white text-center text-h6">{{ unitType.label }}</div>
         </q-card>
       </q-list>
     </div>
 
-
-
-
     <!-- units card - section 2 -->
     <div class="row justify-between" style="height: 100%;">
-
       <div class="col-md-6 col-12 row justify-center">
         <q-img src="~src/assets/resources/home/unit-types/7.png" class="side-image"/>
       </div>
-
       <q-card flat class="
         bg-transparent
         text-body1
@@ -254,10 +238,8 @@
           <div class="text-brown"><b>Living Spaces</b></div>
           <div class="text-h3 q-mb-md">Expertly Designed</div>
           <div class="q-mb-lg">Architects Boogertman & Partners have spent countless hours configuring each of the living spaces at The Web. Ergonomically designed units all feature a kitchenette, private bathroom, study desk, and open-plan living spaces with premium fittings and fixtures.</div>
-
           <CustomButton v-if="$q.screen.gt.sm" color="brown" text-color="white" customStyle="width: 25%" label="Make an enquiry" @click="scrollToSection('contact-section')" />
           <CustomButton v-else color="brown" text-color="white" customStyle="width: 100%" label="Make an enquiry" @click="scrollToSection('contact-section')" />
-
         </q-card-section>
       </q-card>
     </div>
